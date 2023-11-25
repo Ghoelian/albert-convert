7836eee38a086051b9a64ba15d20febc95effbb0
Julian Vos <post@julianvos.nl>
2023-11-25 12:19:21 +0100

    fix: add depth limit to recursive toBase function, and fix QString.append not working on Ubuntu container

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -24,19 +24,23 @@ int bases[] = {
 string baseNames[] = {"", "", "Binary", "Base-3", "Base-4", "Base-5", "Base-6", "Base-7", "Octal", "Base-9", "Decimal",
                       "Base-11", "Base-12", "Base-13", "Base-14", "Base-15", "Hexadecimal"};
 
-string toBase(unsigned n, unsigned base) {
+string toBase(unsigned n, unsigned base, unsigned depth = 1) {
+    if (depth >= 34) {
+        throw std::invalid_argument("Recursion limit exceeded");
+    }
+
     const string chars = "0123456789ABCDEF";
-    return n < base ? chars.substr(n, 1) : toBase(n / base, base) + chars[n % base];
+    return n < base ? chars.substr(n, 1) : toBase(n / base, base, depth + 1) + chars[n % base];
 }
 
 static shared_ptr<Item> buildItem(int base, const unsigned long long &query) {
-    QString id = "base-";
+    string id = "base-";
     id.append(to_string(base));
 
     QString text = QString::fromStdString(toBase(query, base));
 
     return StandardItem::make(
-            id,
+            QString::fromStdString(id),
             text,
             QString::fromStdString(baseNames[base]),
             QStringList({":convert"}),
@@ -49,14 +53,14 @@ static shared_ptr<Item> buildItem(int base, const unsigned long long &query) {
 void Plugin::handleTriggerQuery(TriggerQuery *query) const {
     string queryString = query->string().toStdString();
 
-    char* p;
+    char *p;
     unsigned long long convertedQuery = strtoull(queryString.c_str(), &p, 10);
 
     if (*p) {
-        throw "Invalid input";
+        throw std::invalid_argument("Invalid input");
     }
 
-    for (int i = 0; i < 14; i++) {
-        query->add(buildItem(bases[i], convertedQuery));
+    for (int base: bases) {
+        query->add(buildItem(base, convertedQuery));
     }
 }
